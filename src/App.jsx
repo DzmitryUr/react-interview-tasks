fc3f67676473177940c09fb1928745cf23a0a9bb
Dzmitry <dimaau1@gmail.com>
2025-01-17 20:31:19 +0200

    image carousel challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { BooksList } from './components/BooksList';
+import { ImageCarousel } from './components/ImageCarousel';
 import './App.css';
+
+const images = [
+  'https://picsum.photos/id/10/600/400',
+  'https://picsum.photos/id/11/600/400',
+  'https://picsum.photos/id/12/600/400',
+  'https://picsum.photos/id/13/600/400',
+  'https://picsum.photos/id/15/600/400',
+];
 
 function App() {
   return (
     <>
       <h1>React Interview Challenges</h1>
-      <BooksList />
+      <ImageCarousel images={images} />
     </>
   );
 }
